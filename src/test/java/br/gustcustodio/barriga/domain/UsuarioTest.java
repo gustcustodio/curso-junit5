66c6499e9574e27250f853920e98a457fcb2cc8b
gustcustodio <gustcustodio@gmail.com>
2026-03-04 06:24:48 -0300

    organizando assertivas

--- src/test/java/br/gustcustodio/barriga/domain/UsuarioTest.java
+++ src/test/java/br/gustcustodio/barriga/domain/UsuarioTest.java
@@ -4,17 +4,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DisplayName("Domínio: Usuário")
 public class UsuarioTest {
 
     @Test
     @DisplayName("Deve criar usuário válido")
     public void deveCriarUsuarioValido() {
-        Usuario usuario = new Usuario(1L, "Usuario valido", "usuario@email.com", "123456");
-        Assertions.assertEquals(1L, usuario.getId());
-        Assertions.assertEquals("Usuario valido", usuario.getNome());
-        Assertions.assertEquals("usuario@email.com", usuario.getEmail());
-        Assertions.assertEquals("123456", usuario.getSenha());
+        Usuario usuario = new Usuario(1L, "Usuario Valido", "usuario@email.com", "123456");
+        Assertions.assertAll("Usuario",
+                () -> assertEquals(1L, usuario.getId()),
+                () -> assertEquals("Usuario Valido", usuario.getNome()),
+                () -> assertEquals("usuario@email.com", usuario.getEmail()),
+                () -> assertEquals("123456", usuario.getSenha())
+        );
     }
 
 }
