f50866302a51a03f2696b9610a0c44bbba37d862
gustcustodio <gustcustodio@gmail.com>
2026-03-11 07:06:27 -0300

    memory data

---
+++ src/main/java/br/gustcustodio/barriga/infra/UsuarioMemoryRepository.java
@@ -0,0 +1,52 @@
+package br.gustcustodio.barriga.infra;
+
+import br.gustcustodio.barriga.domain.Usuario;
+import br.gustcustodio.barriga.repositories.UsuarioRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class UsuarioMemoryRepository implements UsuarioRepository {
+
+    private List<Usuario> users;
+    private Long currentId;
+
+    public UsuarioMemoryRepository() {
+        currentId = 0L;
+        users = new ArrayList<>();
+        salvar(new Usuario(null, "User #1", "user1@mail.com", "123456"));
+    }
+
+    @Override
+    public Usuario salvar(Usuario usuario) {
+        Usuario newUser = new Usuario(nextId(), usuario.getNome(), usuario.getEmail(), usuario.getSenha());
+        users.add(newUser);
+        return newUser;
+    }
+
+    @Override
+    public Optional<Usuario> getUserByEmail(String email) {
+        return users.stream()
+                .filter(user -> user.getEmail().equalsIgnoreCase(email))
+                .findFirst();
+    }
+
+    public void printUsers() {
+        System.out.println(users);
+    }
+
+    private Long nextId() {
+        return ++currentId;
+    }
+
+    public static void main(String[] args) {
+        UsuarioMemoryRepository repo = new UsuarioMemoryRepository();
+        repo.printUsers();
+        repo.salvar(new Usuario(null, "Usuário qualquer", "a@a.com", "aaa"));
+        repo.printUsers();
+        repo.salvar(new Usuario(null, "Usuário 3", "a@a.com", "aaa"));
+        repo.printUsers();
+    }
+
+}
