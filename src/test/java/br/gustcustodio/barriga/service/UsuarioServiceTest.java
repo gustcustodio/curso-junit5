f466f5784a3a78861f6f774948ceca2898e032c6
gustcustodio <gustcustodio@gmail.com>
2026-03-16 07:02:25 -0300

    deve retornar usuário por email

--- src/test/java/br/gustcustodio/barriga/service/UsuarioServiceTest.java
+++ src/test/java/br/gustcustodio/barriga/service/UsuarioServiceTest.java
@@ -1,6 +1,7 @@
 package br.gustcustodio.barriga.service;
 
 import br.gustcustodio.barriga.domain.Usuario;
+import br.gustcustodio.barriga.domain.builders.UsuarioBuilder;
 import br.gustcustodio.barriga.repositories.UsuarioRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,17 @@ public class UsuarioServiceTest {
         usuarioService = new UsuarioService(usuarioRepository);
         Optional<Usuario> user = usuarioService.getUserByEmail("mail@mail.com");
         Assertions.assertTrue(user.isEmpty());
+    }
+
+    @Test
+    public void deveRetornarUsuarioPorEmail() {
+        UsuarioRepository usuarioRepository = Mockito.mock(UsuarioRepository.class);
+        usuarioService = new UsuarioService(usuarioRepository);
+
+        Mockito.when(usuarioRepository.getUserByEmail("mail@mail.com")).thenReturn(Optional.of(UsuarioBuilder.umUsuario().agora()));
+
+        Optional<Usuario> user = usuarioService.getUserByEmail("mail@mail.com");
+        Assertions.assertFalse(user.isEmpty());
     }
 
 }
