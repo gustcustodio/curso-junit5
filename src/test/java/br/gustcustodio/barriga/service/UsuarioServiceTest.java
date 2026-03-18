86c376f3aec999f0fe27edb4e29fff9d2ef77e19
gustcustodio <gustcustodio@gmail.com>
2026-03-18 06:45:00 -0300

    repetições em mock

--- src/test/java/br/gustcustodio/barriga/service/UsuarioServiceTest.java
+++ src/test/java/br/gustcustodio/barriga/service/UsuarioServiceTest.java
@@ -26,12 +26,22 @@ public class UsuarioServiceTest {
         UsuarioRepository usuarioRepository = Mockito.mock(UsuarioRepository.class);
         usuarioService = new UsuarioService(usuarioRepository);
 
-        Mockito.when(usuarioRepository.getUserByEmail("mail@mail.com")).thenReturn(Optional.of(UsuarioBuilder.umUsuario().agora()));
+        Mockito.when(usuarioRepository.getUserByEmail("mail@mail.com"))
+                .thenReturn(Optional.of(UsuarioBuilder.umUsuario().agora()), Optional.of(UsuarioBuilder.umUsuario().agora()), null);
 
         Optional<Usuario> user = usuarioService.getUserByEmail("mail@mail.com");
+        System.out.println(user);
         Assertions.assertTrue(user.isPresent());
+        user = usuarioService.getUserByEmail("mail123123@mail.com");
+        System.out.println(user);
+        user = usuarioService.getUserByEmail("mail@mail.com");
+        System.out.println(user);
+        user = usuarioService.getUserByEmail("mail@mail.com");
+        System.out.println(user);
 
-        Mockito.verify(usuarioRepository, Mockito.atLeastOnce()).getUserByEmail("mail@mail.com");
+        Mockito.verify(usuarioRepository, Mockito.times(3)).getUserByEmail("mail@mail.com");
+        Mockito.verify(usuarioRepository, Mockito.times(1)).getUserByEmail("mail123123@mail.com");
+//        Mockito.verify(usuarioRepository, Mockito.atLeastOnce()).getUserByEmail("mail@mail.com");
         Mockito.verify(usuarioRepository, Mockito.never()).getUserByEmail("outro@mail.com");
         Mockito.verifyNoMoreInteractions(usuarioRepository);
     }
