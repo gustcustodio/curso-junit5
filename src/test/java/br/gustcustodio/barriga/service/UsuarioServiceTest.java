1175277aa0b21070485081f6be76c562e734b9cd
gustcustodio <gustcustodio@gmail.com>
2026-03-24 07:01:24 -0300

    deve rejeitar usuário inexistente

--- src/test/java/br/gustcustodio/barriga/service/UsuarioServiceTest.java
+++ src/test/java/br/gustcustodio/barriga/service/UsuarioServiceTest.java
@@ -1,7 +1,7 @@
 package br.gustcustodio.barriga.service;
 
 import br.gustcustodio.barriga.domain.Usuario;
-import br.gustcustodio.barriga.domain.builders.UsuarioBuilder;
+import br.gustcustodio.barriga.domain.exceptions.ValidationException;
 import br.gustcustodio.barriga.repositories.UsuarioRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,6 +13,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static br.gustcustodio.barriga.domain.builders.UsuarioBuilder.umUsuario;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -33,29 +35,40 @@ public class UsuarioServiceTest {
 
     @Test
     public void deveRetornarUsuarioPorEmail() {
-        when(usuarioRepository.getUserByEmail("mail@mail.com"))
-                .thenReturn(Optional.of(UsuarioBuilder.umUsuario().agora()));
+        when(usuarioRepository.getUserByEmail("mail@mail.com")).thenReturn(Optional.of(umUsuario().agora()));
 
         Optional<Usuario> user = usuarioService.getUserByEmail("mail@mail.com");
         System.out.println(user);
         Assertions.assertTrue(user.isPresent());
 
         verify(usuarioRepository, Mockito.atLeastOnce()).getUserByEmail("mail@mail.com");
-        verify(usuarioRepository, Mockito.never()).getUserByEmail("outro@mail.com");
+        verify(usuarioRepository, never()).getUserByEmail("outro@mail.com");
         Mockito.verifyNoMoreInteractions(usuarioRepository);
     }
 
     @Test
     public void deveSalvarUsuarioComSucesso() {
-        Usuario userToSave = UsuarioBuilder.umUsuario().comId(null).agora();
+        Usuario userToSave = umUsuario().comId(null).agora();
 
-        when(usuarioRepository.salvar(userToSave)).thenReturn(UsuarioBuilder.umUsuario().agora());
+        when(usuarioRepository.salvar(userToSave)).thenReturn(umUsuario().agora());
 
         Usuario savedUser = usuarioService.salvar(userToSave);
         Assertions.assertNotNull(savedUser.getId());
 
         verify(usuarioRepository).getUserByEmail(userToSave.getEmail());
 //		verify(repository).salvar(userToSave);
+    }
+
+    @Test
+    public void deveRejeitarUsuarioExistente() {
+        Usuario userToSave = umUsuario().comId(null).agora();
+        when(usuarioRepository.getUserByEmail(userToSave.getEmail())).thenReturn(Optional.of(umUsuario().agora()));
+
+        ValidationException ex = Assertions.assertThrows(ValidationException.class, () -> usuarioService.salvar(userToSave));
+
+        Assertions.assertTrue(ex.getMessage().endsWith("já cadastrado!"));
+
+        verify(usuarioRepository, never()).salvar(userToSave);
     }
 
 }
